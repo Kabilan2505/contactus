e16c0576d278220c4729a65eac225c9ea249d3ab
kabilan2505 <kabilankannan2505@gmail.com>
2024-03-26 18:16:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {ContactUsComponent} from "./contact-us/contact-us.component";
+import {MatCardModule} from "@angular/material/card";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ContactUsComponent, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/contact-us/contact-us.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatIcon} from "@angular/material/icon";
+import {MatCard, MatCardContent, MatCardHeader, MatCardModule} from "@angular/material/card";
+import {MatDivider} from "@angular/material/divider";
+import {FlexModule} from "@angular/flex-layout";
+
+@Component({
+  selector: 'app-contact-us',
+  standalone: true,
+  imports: [
+    MatFormField,
+    MatInput,
+    MatIcon,
+    MatCardModule,
+    MatCard,
+    MatLabel,
+    MatCardHeader,
+    MatDivider,
+    MatCardContent,
+    FlexModule,
+
+  ],
+  templateUrl: './contact-us.component.html',
+  styleUrl: './contact-us.component.css'
+})
+export class ContactUsComponent {
+
+}
