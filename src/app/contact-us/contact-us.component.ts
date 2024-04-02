bf8ccd23c19b3ba60080714b6033b7a30f78b7c1
kabilan2505 <kabilankannan2505@gmail.com>
2024-04-02 16:07:55 +0530

    after changes

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {Component, OnInit} from '@angular/core';
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatIcon} from "@angular/material/icon";
 import {MatCard, MatCardContent, MatCardHeader, MatCardModule} from "@angular/material/card";
 import {MatDivider} from "@angular/material/divider";
 import {FlexModule} from "@angular/flex-layout";
 import {MatButton} from "@angular/material/button";
+import {MatToolbar} from "@angular/material/toolbar";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl, FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
 
+  Validators,
+} from "@angular/forms";
+import {NgIf} from "@angular/common";
+
+export class UserEnquiryRequest{
+  firstName!: string
+  lastName!: string
+  email!: string
+  phoneNumber!: string
+  message!: string
+}
 @Component({
   selector: 'app-contact-us',
   standalone: true,
@@ -22,11 +40,43 @@ import {MatButton} from "@angular/material/button";
     MatCardContent,
     FlexModule,
     MatButton,
+    MatToolbar,
+    ReactiveFormsModule,
+    FormsModule,
+    MatError,
+    NgIf,
 
   ],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit {
+  userEnquiry !: FormGroup;
+userEnquiryRequest: UserEnquiryRequest= new UserEnquiryRequest();
 
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+
+  ngOnInit(): void {
+    this.userEnquiry =this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.maxLength(20), Validators.minLength(4), Validators.pattern('[a-zA-Z]+') ]],
+      lastName:['', [Validators.required, Validators.maxLength(20), Validators.pattern('[a-zA-Z]+')]],
+      email: ['',[Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12), Validators.pattern('[0-9]+')]],
+      message: ['', [Validators.required]]
+    })
+  }
+
+  onSubmit() {
+    if (this.userEnquiry.valid){
+      this.userEnquiryRequest.firstName = this.userEnquiry.value.firstName;
+      this.userEnquiryRequest.lastName = this.userEnquiry.value.lastName;
+      this.userEnquiryRequest.email = this.userEnquiry.value.email;
+      this.userEnquiryRequest.phoneNumber = this.userEnquiry.value.phoneNumber;
+      this.userEnquiryRequest.message =this.userEnquiry.value.message;
+      console.log('data',this.userEnquiryRequest)
+    }
+
+  }
 }
